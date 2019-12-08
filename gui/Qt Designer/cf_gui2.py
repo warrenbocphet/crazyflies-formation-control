3897f590a7d194ef51a2a21e55acddd6618f1e1d
Anh Tran <tranluuvietanh@gmail.com>
2019-12-08 15:29:43 +1030

    Add 'send command function' to the GUI.

--- gui/Qt Designer/cf_gui2.py
+++ gui/Qt Designer/cf_gui2.py
@@ -95,78 +95,111 @@ class cf_control():
 class Ui_MainWindow(object):
 	def initDrones(self):
 		self.cfs = []
+		self.selected_cf = None
 
 	def setupUi(self, MainWindow):
 		MainWindow.setObjectName("MainWindow")
-		MainWindow.resize(508, 315)
+		MainWindow.resize(488, 393)
 		icon = QtGui.QIcon()
 		icon.addPixmap(QtGui.QPixmap("../media/uoa_logo.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
 		MainWindow.setWindowIcon(icon)
 		self.centralwidget = QtWidgets.QWidget(MainWindow)
 		self.centralwidget.setObjectName("centralwidget")
+		self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+		self.gridLayout.setObjectName("gridLayout")
 		self.btn_scan = QtWidgets.QPushButton(self.centralwidget)
-		self.btn_scan.setGeometry(QtCore.QRect(9, 30, 91, 25))
 		self.btn_scan.setObjectName("btn_scan")
+		self.gridLayout.addWidget(self.btn_scan, 0, 0, 1, 1)
 		self.combobox_drone = QtWidgets.QComboBox(self.centralwidget)
-		self.combobox_drone.setGeometry(QtCore.QRect(110, 30, 151, 25))
 		self.combobox_drone.setObjectName("combobox_drone")
-		self.btn_snd_cmd = QtWidgets.QPushButton(self.centralwidget)
-		self.btn_snd_cmd.setGeometry(QtCore.QRect(360, 190, 112, 25))
-		self.btn_snd_cmd.setObjectName("btn_snd_cmd")
+		self.gridLayout.addWidget(self.combobox_drone, 0, 1, 1, 1)
+		spacerItem = QtWidgets.QSpacerItem(3, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+		self.gridLayout.addItem(spacerItem, 0, 2, 1, 2)
+		spacerItem1 = QtWidgets.QSpacerItem(97, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+		self.gridLayout.addItem(spacerItem1, 0, 4, 1, 2)
 		self.label_roll = QtWidgets.QLabel(self.centralwidget)
-		self.label_roll.setGeometry(QtCore.QRect(300, 30, 67, 17))
 		self.label_roll.setObjectName("label_roll")
+		self.gridLayout.addWidget(self.label_roll, 0, 6, 1, 1)
+		self.edit_roll = QtWidgets.QLineEdit(self.centralwidget)
+		self.edit_roll.setObjectName("edit_roll")
+		self.gridLayout.addWidget(self.edit_roll, 0, 7, 1, 1)
+		self.btn_connectall = QtWidgets.QPushButton(self.centralwidget)
+		self.btn_connectall.setObjectName("btn_connectall")
+		self.gridLayout.addWidget(self.btn_connectall, 1, 0, 1, 1)
+		spacerItem2 = QtWidgets.QSpacerItem(89, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+		self.gridLayout.addItem(spacerItem2, 1, 1, 1, 2)
+		spacerItem3 = QtWidgets.QSpacerItem(97, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+		self.gridLayout.addItem(spacerItem3, 1, 3, 1, 2)
 		self.label_pitch = QtWidgets.QLabel(self.centralwidget)
-		self.label_pitch.setGeometry(QtCore.QRect(300, 70, 67, 17))
 		self.label_pitch.setObjectName("label_pitch")
+		self.gridLayout.addWidget(self.label_pitch, 1, 6, 1, 1)
+		self.edit_pitch = QtWidgets.QLineEdit(self.centralwidget)
+		self.edit_pitch.setObjectName("edit_pitch")
+		self.gridLayout.addWidget(self.edit_pitch, 1, 7, 1, 1)
+		spacerItem4 = QtWidgets.QSpacerItem(81, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+		self.gridLayout.addItem(spacerItem4, 2, 0, 1, 1)
+		spacerItem5 = QtWidgets.QSpacerItem(89, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+		self.gridLayout.addItem(spacerItem5, 2, 1, 1, 2)
+		spacerItem6 = QtWidgets.QSpacerItem(91, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+		self.gridLayout.addItem(spacerItem6, 2, 4, 1, 1)
 		self.label_yawrate = QtWidgets.QLabel(self.centralwidget)
-		self.label_yawrate.setGeometry(QtCore.QRect(300, 110, 67, 17))
 		self.label_yawrate.setObjectName("label_yawrate")
+		self.gridLayout.addWidget(self.label_yawrate, 2, 6, 1, 1)
+		self.edit_yawrate = QtWidgets.QLineEdit(self.centralwidget)
+		self.edit_yawrate.setObjectName("edit_yawrate")
+		self.gridLayout.addWidget(self.edit_yawrate, 2, 7, 1, 1)
+		spacerItem7 = QtWidgets.QSpacerItem(81, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+		self.gridLayout.addItem(spacerItem7, 3, 0, 1, 1)
+		spacerItem8 = QtWidgets.QSpacerItem(89, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+		self.gridLayout.addItem(spacerItem8, 3, 1, 1, 2)
+		spacerItem9 = QtWidgets.QSpacerItem(91, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+		self.gridLayout.addItem(spacerItem9, 3, 4, 1, 1)
 		self.label_thrust = QtWidgets.QLabel(self.centralwidget)
-		self.label_thrust.setGeometry(QtCore.QRect(300, 150, 67, 17))
 		self.label_thrust.setObjectName("label_thrust")
+		self.gridLayout.addWidget(self.label_thrust, 3, 6, 1, 1)
+		self.edit_thrust = QtWidgets.QLineEdit(self.centralwidget)
+		self.edit_thrust.setObjectName("edit_thrust")
+		self.gridLayout.addWidget(self.edit_thrust, 3, 7, 1, 1)
+		spacerItem10 = QtWidgets.QSpacerItem(179, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+		self.gridLayout.addItem(spacerItem10, 4, 0, 1, 3)
+		spacerItem11 = QtWidgets.QSpacerItem(91, 22, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+		self.gridLayout.addItem(spacerItem11, 4, 4, 1, 1)
+		self.btn_snd_cmd = QtWidgets.QPushButton(self.centralwidget)
+		self.btn_snd_cmd.setObjectName("btn_snd_cmd")
+		self.gridLayout.addWidget(self.btn_snd_cmd, 4, 5, 1, 3)
 		self.btn_exit = QtWidgets.QPushButton(self.centralwidget)
-		self.btn_exit.setGeometry(QtCore.QRect(10, 260, 89, 25))
 		self.btn_exit.setAutoFillBackground(False)
 		self.btn_exit.setObjectName("btn_exit")
-		self.input_roll = QtWidgets.QTextEdit(self.centralwidget)
-		self.input_roll.setGeometry(QtCore.QRect(370, 20, 104, 31))
-		self.input_roll.setObjectName("input_roll")
-		self.input_pitch = QtWidgets.QTextEdit(self.centralwidget)
-		self.input_pitch.setGeometry(QtCore.QRect(370, 60, 104, 31))
-		self.input_pitch.setObjectName("input_pitch")
-		self.input_yawrate = QtWidgets.QTextEdit(self.centralwidget)
-		self.input_yawrate.setGeometry(QtCore.QRect(370, 100, 104, 31))
-		self.input_yawrate.setObjectName("input_yawrate")
-		self.input_thrust = QtWidgets.QTextEdit(self.centralwidget)
-		self.input_thrust.setGeometry(QtCore.QRect(370, 140, 104, 31))
-		self.input_thrust.setObjectName("input_thrust")
-		self.btn_connectall = QtWidgets.QPushButton(self.centralwidget)
-		self.btn_connectall.setGeometry(QtCore.QRect(10, 70, 91, 51))
-		self.btn_connectall.setObjectName("btn_connectall")
+		self.gridLayout.addWidget(self.btn_exit, 5, 0, 1, 1)
+		spacerItem12 = QtWidgets.QSpacerItem(89, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+		self.gridLayout.addItem(spacerItem12, 5, 1, 1, 2)
+		spacerItem13 = QtWidgets.QSpacerItem(273, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+		self.gridLayout.addItem(spacerItem13, 5, 4, 1, 4)
 		MainWindow.setCentralWidget(self.centralwidget)
-		self.statusbar = QtWidgets.QStatusBar(MainWindow)
-		self.statusbar.setObjectName("statusbar")
-		MainWindow.setStatusBar(self.statusbar)
+		self.menuBar = QtWidgets.QMenuBar(MainWindow)
+		self.menuBar.setGeometry(QtCore.QRect(0, 0, 488, 22))
+		self.menuBar.setObjectName("menuBar")
+		MainWindow.setMenuBar(self.menuBar)
 
 		self.retranslateUi(MainWindow)
 		QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
 		self.btn_exit.clicked.connect(self.btn_exit_clicked)
 		self.btn_scan.clicked.connect(self.btn_scan_clicked)
-		self.btn_connectall.clicked.connect(self.btn_connect_all)
+		self.btn_connectall.clicked.connect(self.btn_connect_all_clicked)
+		self.btn_snd_cmd.clicked.connect(self.btn_snd_cmd_clicked)
 
 	def retranslateUi(self, MainWindow):
 		_translate = QtCore.QCoreApplication.translate
 		MainWindow.setWindowTitle(_translate("MainWindow", "Crazyflie Multiagent"))
 		self.btn_scan.setText(_translate("MainWindow", "Scan"))
-		self.btn_snd_cmd.setText(_translate("MainWindow", "Send command"))
 		self.label_roll.setText(_translate("MainWindow", "Roll"))
+		self.btn_connectall.setText(_translate("MainWindow", "Connect all"))
 		self.label_pitch.setText(_translate("MainWindow", "Pitch"))
 		self.label_yawrate.setText(_translate("MainWindow", "Yaw rate"))
 		self.label_thrust.setText(_translate("MainWindow", "Thrust"))
+		self.btn_snd_cmd.setText(_translate("MainWindow", "Send command"))
 		self.btn_exit.setText(_translate("MainWindow", "EXIT"))
-		self.btn_connectall.setText(_translate("MainWindow", "Connect all"))
 
 	def btn_exit_clicked(self):
 		print("Program closed.")
@@ -190,7 +223,7 @@ class Ui_MainWindow(object):
 		else:
 			print('No Crazyflies found :(')
 
-	def btn_connect_all(self):
+	def btn_connect_all_clicked(self):
 		for i in range(len(self.radio_items)):
 			cf = cf_control(self.radio_items[i])
 			self.cfs.append(cf)
@@ -198,9 +231,21 @@ class Ui_MainWindow(object):
 				continue
 			cf = None
 
-	# def btn_send_cmd(self):
+		if (len(self.cfs) > 0):
+			self.selected_cf = self.cfs[0]
 
+	def comboDrones_index_changed(self):
+		self.selected_cf = cfs[self.combobox_drone.currentIndex()]
 
+	def btn_snd_cmd_clicked(self):
+		roll = int(self.edit_roll.text())
+		pitch = int(self.edit_pitch.text())
+		yawrate = int(self.edit_yawrate.text())
+		thrust = int(self.edit_thrust.text())
+
+		self.selected_cf.new_command = 1
+		self.selected_cf.set_control_command_param(roll, pitch, yawrate, thrust)
+		self.selected_cf.send_control_command()
 
 if __name__ == "__main__":
 	# Initialize the low-level drivers
